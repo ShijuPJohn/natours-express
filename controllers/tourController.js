5d262227136e291096dbe08cd828c28b6689cede
ShijuPJohn <shijupjohn4u@gmail.com>
2021-05-09 21:14:04 +0530

    feat: complete schema

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,94 +1,96 @@
 const Tour = require('./../models/tourModel');
 
-
-// exports.checkBody = (req, res, next) => {
-//   const reqBody = req.body;
-//   if ((!reqBody.name) || (!reqBody.price)) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price'
-//     });
-//   }
-//   next();
-// };
 exports.getAllTours = (async (req, res) => {
-  try {
-    const tours = await Tour.find();
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours: tours
-      }
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: 'error',
-      message: 'some error occurred while trying to access db'
-    });
-  }
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours: tours
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: 'error',
+            message: 'some error occurred while trying to access db'
+        });
+    }
 });
 
 exports.getTour = (async (req, res) => {
-  try {
-    const tours = await Tour.findById(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tours: tours
-      }
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: 'error',
-      message: 'some error occurred while trying to access db'
-    });
-  }
+    try {
+        const tours = await Tour.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tours: tours
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: 'error',
+            message: 'some error occurred while trying to access db'
+        });
+    }
 
 });
 
 exports.createTour = (async (req, res) => {
-  console.log('inside createTour()');
-  try {
-    const reqBody = req.body;
-    const newTour = await Tour.create(reqBody);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: 'error'
-    });
-  }
+    try {
+        const reqBody = req.body;
+        const newTour = await Tour.create(reqBody);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'error'
+        });
+    }
 });
 
-exports.deleteTour = ((req, res) => {
-  // let tour = tours.find(tour => tour.id === parseInt(req.params.id));
-
-  res.status(204).json({
-    status: 'success',
-    data: {
-      tour: null
+exports.deleteTour = (async (req, res) => {
+    try {
+        const result = await Tour.findByIdAndDelete(req.params.id);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                result: result
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'error'
+        });
     }
-  });
 
 });
 
-exports.updateTour = ((req, res) => {
-  try {
-
-  } catch (e) {
-
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<updated tour here>'
+exports.updateTour = (async (req, res) => {
+    console.log(req.body)
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'error'
+        });
     }
-  });
+
 
 });
